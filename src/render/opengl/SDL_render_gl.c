eebe5772d9cd5e5a2f3ec3d5d0bd46d6fb6e9d2b
3UR <76826837+3UR@users.noreply.github.com>
2025-03-04 10:39:30 +1000

    __WINRT__ -> SDL_PLATFORM_WINRT

--- src/render/opengl/SDL_render_gl.c
+++ src/render/opengl/SDL_render_gl.c
@@ -1789,7 +1789,7 @@ static bool GL_CreateRenderer(SDL_Renderer *renderer, SDL_Window *window, SDL_Pr
         goto error;
     }
 
-#if !defined SDL_VIDEO_VITA_PVR_OGL && !defined __WINRT__
+#if !defined SDL_VIDEO_VITA_PVR_OGL && !defined SDL_PLATFORM_WINRT
     SDL_SyncWindow(window);
     window_flags = SDL_GetWindowFlags(window);
     if (!(window_flags & SDL_WINDOW_OPENGL) ||
@@ -2048,7 +2048,7 @@ error:
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, profile_mask);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, major);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, minor);
-#ifndef __WINRT__
+#ifndef SDL_PLATFORM_WINRT
         SDL_RecreateWindow(window, window_flags);
 #endif
         SDL_SetError("%s", error);
